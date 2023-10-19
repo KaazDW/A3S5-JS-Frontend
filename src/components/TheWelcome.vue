934d39d6a773a98162bc9b80ca03f621576fbb46
/kaaz <cz2600036@gmail.com>
2023-10-19 16:24:50 +0200

    [dev] 2.todolist

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,10 +8,26 @@ import SupportIcon from './icons/IconSupport.vue'
 
 import { ref } from 'vue'
 
-const count = ref(0)
-const incrementCounter = () => {
-  count.value++
-}
+  const count = ref(0)
+  const incrementCounter = () => {
+    count.value++
+  }
+
+  const tasks = ref([
+    { text: 'Exemple de tâche', completed: false }
+  ]);
+  const newTask = ref('');
+
+  const addTask = () => {
+    if (newTask.value !== '') {
+      tasks.value.push({ text: newTask.value, completed: false });
+      newTask.value = '';
+    }
+  };
+
+  const removeTask = (index) => {
+    tasks.value.splice(index, 1);
+  };
 </script>
 
 <template>
@@ -33,11 +49,24 @@ const incrementCounter = () => {
     </template>
     <template #heading>2. TodoList</template>
     <div class="todo">
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          <button @click="removeTask(index)">Supprimer</button>
+          <input type="checkbox" v-model="task.completed" />
+          {{ task.text }}
+        </li>
+      </ul>
+      <input v-model="newTask" placeholder="Ajouter une nouvelle tâche" />
+      <button @click="addTask">Ajouter</button>
 
     </div>
   </WelcomeItem>
 </template>
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+*{
+  font-family: 'Poppins', sans-serif;
+}
 // 1.Increment
   .incr{
     display: flex;
@@ -54,5 +83,31 @@ const incrementCounter = () => {
     padding: 5px 10px;
     border-radius: 5px;
     margin: 0 5px;
+  }
+
+//  2.Todo
+  .todo{
+    ul{
+      list-style: none;
+      padding: 0;
+      li{
+        margin: 5px 0;
+        button{
+          padding: 3px 5px;
+          margin-right: 10px;
+        }
+        input{
+          margin: 0 2px;
+        }
+      }
+    }
+    input{
+      padding: 5px;
+      border-radius: 5px;
+    }
+    button{
+      padding: 5px;
+      margin: 0 5px;
+    }
   }
 </style>
